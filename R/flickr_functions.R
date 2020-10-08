c4e097c0fe51199a6464599c0df351b31c78d8a2
mattkerlogue <matt.kerlogue@me.com>
2020-10-08 20:27:35 +0100

    add photo scoring and selection functions

--- R/flickr_functions.R
+++ R/flickr_functions.R
@@ -45,7 +45,7 @@ flickr_get_photo_info <- function(key = NULL, photo_id, photo_secret) {
 
 # Get the 100 closest photos to the desired position and pull info
 flickr_get_photo_list <- function(key = NULL, lat, long) {
-
+  
   if (is.null(key)) {
     key <- Sys.getenv("FLICKR_API_KEY")
     if (key == "") {
@@ -115,6 +115,102 @@ flickr_get_photo_list <- function(key = NULL, lat, long) {
     filter(!before_sunrise)
   
   return(p)
+  
+}
+
+# count canal words
+canal_word_count <- function(string) {
+  
+  canal_words <- c("canal", "lock", "water", "boat", "gate", "bird", "duck", 
+                   "swan", "river", "aqueduct", "towpath", "barge", "keeper",
+                   "tunnel")
+  
+  counter <- 0
+  
+  for (word in canal_words) {
+    x <- str_detect(tolower(string), word)
+    counter <- counter + x
+  }
+  
+  return(counter)
+  
+}
+
+# score photos
+# The scoring algorithm takes into account:
+#   * the approx length of titles and 
+#   descriptions (descriptions are logged to benefit titles and not to 
+#   over-reward verbose descriptions),
+#   * the number of canal related words used this seeks the range of canal 
+#   words used rather than the pure count, this is then squared to make it
+#   give it a high weighting,
+#   * whether the time is during 'golden hour' for the location (if so, then 
+#   the algorithm in effect doubles the score for that photo),
+#   * distance from the location (the flickr api return is supposed to provide 
+#   a list that is sorted by distance, however am unsure about this) so this is 
+#   square rooted to reduce variance
+#   * time offset from today, more recent photos are preferred but this 
+#   censored (so that photos older than 5000 days are excluded) and is scaled 
+#   to between 1 and 5 to reduce impact
+#      
+#   The final score is a product of these different components:
+#       SCORE = WORD_SCORE * DIST_SCORE * GOLDEN_HOUR * TIME_OFFSET * CANAL_WORD_SCORE
+#   
+flickr_photo_score <- function(df) {
+  
+  n_df <- df %>%
+    select(id, owner, title, description, date, goldenhour) %>%
+    mutate(
+      title_words = map_dbl(title, ~max(str_count(., " "),1)),
+      desc_words = str_count(description, " "),
+      total_words = title_words + desc_words,
+      desc_words2 = log10(map_dbl(description, ~max(str_count(., " "),1))),
+      word_score = pmap_dbl(list(title_words, desc_words2), sum, na.rm = TRUE),
+      canal_words = canal_word_count(title) + canal_word_count(description),
+      canal_word_score = map_dbl(canal_words^2, ~max(., 1)),
+      gold = if_else(goldenhour, 2, 1),
+      offset = Sys.time() - date
+      ) %>%
+    filter(offset <= 5000) %>%
+    mutate(
+      distance = row_number(),
+      dist_rev = rev(sqrt(distance))
+    ) %>%
+    arrange(offset) %>%
+    mutate(
+      offset_rev = rev(sqrt(as.numeric(offset))),
+      alt_off = scales::rescale(as.numeric(offset), to = c(5,1)),
+      final_score = word_score * dist_rev * gold * alt_off * canal_word_score
+      ) %>%
+    select(id, owner, final_score) %>%
+    arrange(-final_score)
+  
+  return(n_df)
+  
+}
+
+# pick and prep photo for tweet
+flickr_pick_photo <- function(df) {
+  
+  scored_df <- flickr_photo_score(df)
+  
+  n_df <- df %>% 
+    inner_join(scored_df, by = c("id", "owner")) %>%
+    drop_na(final_score) %>%
+    filter(final_score == max(final_score))
+  
+  photo <- as.list(n_df)
+  
+  photo$photo_url <- paste("https://www.flickr.com/photos", 
+                           photo$owner, 
+                           photo$id,
+                           sep = "/")
+  
+  photo$img_url <- paste0("https://live.staticflickr.com/",
+                         photo$server, "/",
+                         photo$id,"_",photo$secret,".jpg")
+  
+  return(photo)
   
 }
 
