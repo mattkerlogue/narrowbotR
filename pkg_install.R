a44781c954578f13762cd45768ac8c0eabb28bef
mattkerlogue <matt.kerlogue@me.com>
2020-10-09 02:12:51 +0100

    package install functions

---
+++ pkg_install.R
@@ -0,0 +1,23 @@
+
+# packages for regular running
+install_runner_packages <- function() {
+
+  pkgs <- c("dplyr", "purrr", "readr", "tidyr", "rtweet",
+            "janitor", "lubridate", "stringr", "suncalc")
+  
+  install.packages(pkgs)
+  invisible(NULL)
+  
+}
+
+# packages for maintenance
+# as runner but with sf
+install_runner_packages <- function() {
+
+pkgs <- c("dplyr", "purrr", "readr", "tidyr", "rtweet", "sf",
+          "janitor", "lubridate", "stringr", "suncalc")
+
+install.packages(pkgs)
+invisible(NULL)
+
+}
