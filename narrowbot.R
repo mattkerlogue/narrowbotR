f3c85abe0dc07aa0ca76a8f3160f2b0b9f634784
Matt Kerlogue <matt.kerlogue@me.com>
2022-08-04 17:30:04 +0100

    Update bot for rtweet v1

--- narrowbot.R
+++ narrowbot.R
@@ -1,20 +1,22 @@
 # load functions
+
+cli::cli_progress_step("Load flickr functions")
+
 library(dplyr)
-source("R/post_geo_tweet.R")
 source("R/flickr_functions.R")
 
-message("Stage: functions loaded")
+
+cli::cli_progress_step("Get rtweet token")
 
 # create twitter token
-narrowbotr_token <- rtweet::create_token(
-  app = "narrowbotr",
-  consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
-  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+narrowbotr_token <- rtweet::rtweet_bot(
+  api_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  api_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
   access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
 
-message("Stage: Twitter token created")
+cli::cli_progress_step("Select location")
 
 # load points data
 all_points <- readRDS("data/all_points.RDS")
@@ -26,23 +28,22 @@ place <- all_points %>%
   dplyr::sample_n(1) %>%
   as.list()
 
-message("Stage: Picked a place [", place$name,
-        ", lat: ", place$lat,
-        ", long: ", place$long, "]")
+cli::cli_alert(
+  "Picked {place$name}, lat: {.val {round(place$lat,4)}}, long: {.val {round(place$long,4)}}"
+)
+
+cli::cli_progress_step("Get photo")
 
 place_photos <- flickr_get_photo_list(lat = place$lat, long = place$long)
 
-message("Stage: Got photos")
-
 if (is.null(place_photos)) {
   photo_select <- NULL
-  message("Stage: No photo selected\n")
+  cli::cli_alert_warning("No flickr photo available")
 } else {
   photo_select <- flickr_pick_photo(place_photos)
-  message("Stage: Photo selected\n")
 }
 
-tmp_file <- tempfile()
+tmp_file <- tempfile(fileext = ".jpg")
 
 if (is.null(photo_select)) {
   img_url <- paste0(
@@ -57,7 +58,7 @@ if (is.null(photo_select)) {
 
 download.file(img_url, tmp_file)
 
-message("Stage: Photo downloaded")
+cli::cli_progress_step("Construct tweet")
 
 base_message <- c(
   "📍: ", place$name, "\n",
@@ -70,28 +71,38 @@ base_message <- c(
 
 if (is.null(photo_select)) {
   tweet_text <- base_message
+  alt_msg <- paste0("A satellite image of the area containing ", 
+                    place$name,
+                    ". Provided by MapBox.")
 } else {
   tweet_text <- c(
     base_message, "\n",
     "📸: Photo by ", stringr::str_squish(photo_select$realname), " on Flickr ",
     photo_select$photo_url)
+  alt_msg <- paste0(
+    "A photo taken near ", 
+    place$name,
+    " by ", 
+    stringr::str_squish(photo_select$realname),
+    " on Flickr."
+    )
 }
 
 status_msg <- paste0(tweet_text, collapse = "")
 
-message("Stage: Tweet written")
+cli::cli_progress_step("Post tweet")
 
-
-
-post_geo_tweet(
+rtweet::post_tweet(
   status = status_msg,
-  media = tmp_file,
+  media = tmp_file, 
+  media_alt_text = alt_msg,
   lat = place$lat,
   long = place$long
-  )
+)
 
-message("Stage: Action complete")
+cli::cli_progress_done()
 
-message("\n====\nTweet:\n")
-
-cat(status_msg)
+cli::cli({
+  cli::cli_h1("narrowbot completed")
+  cli::cli_verbatim(status_msg)
+})
