49c9817fb4ca125bc8e5ecfebd0f73670395f46a
Matt Kerlogue <matt.kerlogue@me.com>
2025-05-05 21:33:07 +0100

    Add magick dependency

--- R/pkg_install.R
+++ R/pkg_install.R
@@ -4,7 +4,7 @@ install_packages <- function() {
   
   pkgs <- c("dplyr", "purrr", "readr", "tidyr", "rtoot", "jsonlite",
             "sf", "janitor", "lubridate", "stringr", "data.table", "suncalc",
-            "xml2", "bskyr")
+            "xml2", "bskyr", "magick")
   
   install.packages(pkgs)
   invisible(NULL)
